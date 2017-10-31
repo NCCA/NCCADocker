013df40eb7c67d08dce4e94ab4fbd99588e92aac
jmacey <jmacey@bournemouth.ac.uk>
2017-10-31 14:51:16 +0000

    update build.sh to auto find nvidia drivers and build

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 # get driver version
 nvVersion=`nvidia-smi --query-gpu=driver_version --format=csv | sed -n 2p`
 download="http://us.download.nvidia.com/XFree86/Linux-x86_64/$nvVersion/NVIDIA-Linux-x86_64-$nvVersion.run"
-echo $nvVersion $download
+run="NVIDIA-Linux-x86_64-$nvVersion.run"
 
+docker build --build-arg nvidiaRun=$run --build-arg nvidiaLocation=$download -t jmacey/nccabase --rm . 
