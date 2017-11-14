d59b73883c7a7c8c4d503634da1d995c55927f16
jmacey <jmacey@bournemouth.ac.uk>
2017-11-14 14:36:27 +0000

    updated for nouveau

--- runDocker.sh
+++ runDocker.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker run -it --privileged -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY  $@
+docker run -it --privileged -v /tmp/.X11-unix:/tmp/.X11-unix --device=/dev/dri/card0:/dev/dri/card0 -e DISPLAY=$DISPLAY  -v /home/jmacey/:/home/host/jmacey $@
+
+
+#docker run -it --privileged -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw" $@
