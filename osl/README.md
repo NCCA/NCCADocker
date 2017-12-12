66f41e77b9758105bb58fd82e97977e2274087d9
jmacey <jmacey@bournemouth.ac.uk>
2017-12-12 20:34:40 +0000

    updated osl files

---
+++ osl/README.md
@@ -0,0 +1,4 @@
+# Base Docker OSL Development
+
+This docker file will build a base centos image for building and running OSL 
+
---
+++ osl/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# first check if we have an nvidia driver installed
+dockerFile="none"
+
+if  [ -x "$(command -v nvidia-smi)" &>/dev/null ]; then
+  echo 'Found installed nvidia driver' 
+  dockerFile="Dockerfile"
+fi
+# if not see if default nouveau is installed
+lsmod | grep nouveau &>/dev/null
+
+if [ $? -eq 0 ]; then
+    echo 'Found installed nouveau driver maya will not work with this'
+    exit 1 
+fi
+
+if [ "$dockerFile" == "none"  ]; then
+    echo "There is a good chance you have an ati driver installed"
+    echo "I've not prepared this yet give me a machine with ATI and I will :-)"
+    exit 1
+fi
+
+echo "using $dockerFile"
+
+if [ "$dockerFile" == "Dockerfile" ]
+then
+    # get driver version obviously this is for nvidia as that is what we use
+    # at the university. I will eventually add ati support (but need a card to test)
+    # Run the query and extract the driver info which is on the 2nd line and will be a number such as 384.90 Note docker file will always have the default Uni driver
+    nvVersion=`nvidia-smi --query-gpu=driver_version --format=csv | sed -n 2p`
+
+    # construct two string to pass to docker, one is the dowload url used by wget
+    download="http://us.download.nvidia.com/XFree86/Linux-x86_64/$nvVersion/NVIDIA-Linux-x86_64-$nvVersion.run"
+    # run is the command to chmod +x and run in the docker file
+    run="NVIDIA-Linux-x86_64-$nvVersion.run"
+    # now run docker passing in these arguments
+    #docker build --no-cache --build-arg nvidiaRun=$run --build-arg nvidiaLocation=$download -t jmacey/maya  --rm .
+    echo $run $nvVersion
+    docker build  --no-cache --build-arg nvidiaRun=$run --build-arg nvidiaLocation=$download --rm  -t jmacey/osl  .
+fi
+
+
+
